2c3209a728ee0abbc0614d63209f96b4c4d0e097
Renata Lima <limarenataa@gmail.com>
2021-01-02 16:37:31 -0300

    Estilizando cards Campanha

--- livrocorrente/src/Components/Campanha/index.js
+++ livrocorrente/src/Components/Campanha/index.js
@@ -23,7 +23,25 @@ export default function Campanha(){
                 <p>2 exemplares</p>
                 <p>Biblioteca comunitária X</p>
                 <Button>Doar</Button>
-              </div> 
+              </div>
+          </div>
+          <div className="card-campanha">
+              <img src={require(`./img/pequenoprincipe.png`).default} className="livro-campanha" alt="Capa Livro" />
+              <div className="info-campanha">
+                <h4>O pequeno príncipe</h4>
+                <p>2 exemplares</p>
+                <p>Biblioteca comunitária X</p>
+                <Button>Doar</Button>
+              </div>
+          </div> 
+          <div className="card-campanha">
+              <img src={require(`./img/pequenoprincipe.png`).default} className="livro-campanha" alt="Capa Livro" />
+              <div className="info-campanha">
+                <h4>O pequeno príncipe</h4>
+                <p>2 exemplares</p>
+                <p>Biblioteca comunitária X</p>
+                <Button>Doar</Button>
+              </div>
           </div> 
         </div>
      

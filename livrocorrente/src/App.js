6c96fff131ca0b15417f63ad292581da3be92436
Renata Lima <limarenataa@gmail.com>
2021-01-03 00:56:30 -0300

    Modificando a responsividade mobile

--- livrocorrente/src/App.js
+++ livrocorrente/src/App.js
@@ -14,9 +14,9 @@ function App() {
       <main>
         <Routes />
       </main>
-      <footer>
-        <Footer />
-      </footer>
+        <footer>
+          <Footer />
+        </footer>
     </div>
   </BrowserRouter>
   );
--- livrocorrente/src/Components/Campanha/index.js
+++ livrocorrente/src/Components/Campanha/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Container, Button, Row, Col} from 'react-bootstrap';
+import { Container, Button} from 'react-bootstrap';
 import './campanha.css';
 
 
 export default function Campanha(){
     return(
   
-        <div className="div-campanha">
+        <Container className="div-campanha">
           <div className="card-campanha">
               <img src={require(`./img/pequenoprincipe.png`).default} className="livro-campanha" alt="Capa Livro" />
               <div className="info-campanha">
@@ -25,41 +25,25 @@ export default function Campanha(){
                 <Button>Doar</Button>
               </div>
           </div>
-        </div>
+          <div className="card-campanha">
+              <img src={require(`./img/pequenoprincipe.png`).default} className="livro-campanha" alt="Capa Livro" />
+              <div className="info-campanha">
+                <h4>O pequeno príncipe</h4>
+                <p>2 exemplares</p>
+                <p>Biblioteca comunitária X</p>
+                <Button>Doar</Button>
+              </div>
+          </div>
+          <div className="card-campanha">
+              <img src={require(`./img/pequenoprincipe.png`).default} className="livro-campanha" alt="Capa Livro" />
+              <div className="info-campanha">
+                <h4>O pequeno príncipe</h4>
+                <p>2 exemplares</p>
+                <p>Biblioteca comunitária X</p>
+                <Button>Doar</Button>
+              </div>
+          </div>
+        </Container>
 
     );
 };
-
-// export default function Campanha(){
-//   return(
-
-//       <Container className="div-campanha">
-//         <Row className="row-campanha">
-//           <Col sm={5} className="card-campanha"> {/*Faz com que a coluna fique responsiva, sem precisar estizar com flex via css*/}
-//             <div className="conteudo-campanha">
-//               <img src={require(`./img/pequenoprincipe.png`).default} className="livro-campanha" alt="Capa Livro" />
-//               <div className="info-campanha">
-//                 <h4>O pequeno príncipe</h4>
-//                 <p>2 exemplares</p>
-//                 <p>Biblioteca comunitária X</p>
-//                 <Button>Doar</Button>
-//               </div>
-//             </div>
-//           </Col>
-
-//           <Col sm={5} className="card-campanha"> {/*Faz com que a coluna fique responsiva, sem precisar estizar com flex via css*/}
-//             <div className="conteudo-campanha">
-//               <img src={require(`./img/pequenoprincipe.png`).default} className="livro-campanha" alt="Capa Livro" />
-//               <div className="info-campanha">
-//                 <h4>O pequeno príncipe</h4>
-//                 <p>2 exemplares</p>
-//                 <p>Biblioteca comunitária X</p>
-//                 <Button>Doar</Button>
-//               </div>
-//             </div>
-//           </Col>
-//         </Row> 
-//       </Container>
-
-//   );
-// };

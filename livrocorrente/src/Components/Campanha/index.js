69a9abba098c031218e177ac8981ef5f387ba6ce
Renata Lima <limarenataa@gmail.com>
2021-01-02 21:16:43 -0300

    Testando responsividade com cards bootstrap

--- livrocorrente/src/Components/Campanha/index.js
+++ livrocorrente/src/Components/Campanha/index.js
@@ -1,22 +1,60 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
+import { Container, Button, Row, Col, Card, Image} from 'react-bootstrap';
 import './campanha.css';
 
 
-export default function Campanha(){
-    return(
+// export default function Campanha(){
+//     return(
   
-        <div className="div-campanha">
-          <div className="card-campanha">
-              <img src={require(`./img/pequenoprincipe.png`).default} className="livro-campanha" alt="Capa Livro" />
-              <div className="info-campanha">
-                <h4>O pequeno príncipe</h4>
-                <p>2 exemplares</p>
-                <p>Biblioteca comunitária X</p>
-                <Button>Doar</Button>
-              </div> 
-          </div> 
-          <div className="card-campanha">
+//         <div className="div-campanha">
+//           <div className="card-campanha">
+//               <img src={require(`./img/pequenoprincipe.png`).default} className="livro-campanha" alt="Capa Livro" />
+//               <div className="info-campanha">
+//                 <h4>O pequeno príncipe</h4>
+//                 <p>2 exemplares</p>
+//                 <p>Biblioteca comunitária X</p>
+//                 <Button>Doar</Button>
+//               </div> 
+//           </div> 
+//           <div className="card-campanha">
+//               <img src={require(`./img/pequenoprincipe.png`).default} className="livro-campanha" alt="Capa Livro" />
+//               <div className="info-campanha">
+//                 <h4>O pequeno príncipe</h4>
+//                 <p>2 exemplares</p>
+//                 <p>Biblioteca comunitária X</p>
+//                 <Button>Doar</Button>
+//               </div>
+//           </div>
+//           <div className="card-campanha">
+//               <img src={require(`./img/pequenoprincipe.png`).default} className="livro-campanha" alt="Capa Livro" />
+//               <div className="info-campanha">
+//                 <h4>O pequeno príncipe</h4>
+//                 <p>2 exemplares</p>
+//                 <p>Biblioteca comunitária X</p>
+//                 <Button>Doar</Button>
+//               </div>
+//           </div> 
+//           <div className="card-campanha">
+//               <img src={require(`./img/pequenoprincipe.png`).default} className="livro-campanha" alt="Capa Livro" />
+//               <div className="info-campanha">
+//                 <h4>O pequeno príncipe</h4>
+//                 <p>2 exemplares</p>
+//                 <p>Biblioteca comunitária X</p>
+//                 <Button>Doar</Button>
+//               </div>
+//           </div> 
+//         </div>
+
+//     );
+// };
+
+export default function Campanha(){
+  return(
+
+      <Container className="div-campanha">
+        <Row className="row-campanha">
+          <Col sm={5} className="card-campanha"> {/*Faz com que a coluna fique responsiva, sem precisar estizar com flex via css*/}
+            <div className=" teste conteudo-campanha">
               <img src={require(`./img/pequenoprincipe.png`).default} className="livro-campanha" alt="Capa Livro" />
               <div className="info-campanha">
                 <h4>O pequeno príncipe</h4>
@@ -24,28 +62,22 @@ export default function Campanha(){
                 <p>Biblioteca comunitária X</p>
                 <Button>Doar</Button>
               </div>
-          </div>
-          <div className="card-campanha">
-              <img src={require(`./img/pequenoprincipe.png`).default} className="livro-campanha" alt="Capa Livro" />
-              <div className="info-campanha">
-                <h4>O pequeno príncipe</h4>
-                <p>2 exemplares</p>
-                <p>Biblioteca comunitária X</p>
+            </div>
+          </Col>
+
+          <Col sm={5} className="card-campanha"> {/*Faz com que a coluna fique responsiva, sem precisar estizar com flex via css*/}
+            <Card className="conteudo-campanha">
+              <Image src={require(`./img/pequenoprincipe.png`).default}  rounded fluid className="mx-auto livro-campanha" alt="Capa Livro" />
+              <Card.Body className="info-campanha">
+                <Card.Title>O paaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaequeno príncipe</Card.Title>
+                <Card.Text>2 exemplares</Card.Text>
+                <Card.Text>Biblioteca comunitária X</Card.Text>
                 <Button>Doar</Button>
-              </div>
-          </div> 
-          <div className="card-campanha">
-              <img src={require(`./img/pequenoprincipe.png`).default} className="livro-campanha" alt="Capa Livro" />
-              <div className="info-campanha">
-                <h4>O pequeno príncipe</h4>
-                <p>2 exemplares</p>
-                <p>Biblioteca comunitária X</p>
-                <Button>Doar</Button>
-              </div>
-          </div> 
-        </div>
-     
-      
-      
-    );
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row> 
+      </Container>
+
+  );
 };
